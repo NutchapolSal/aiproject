fe3fadb4c38568d668036efb9d9b88553f44c989
NutchapolSal <Nutchapol.sal@gmail.com>
2024-09-18 11:45:25 +0700

    improve CapAndSave helper app

--- CapAndSave/run.py
+++ CapAndSave/run.py
@@ -4,30 +4,26 @@ from datetime import datetime
 
 def save_image(run, image, count):
     if not run:
-        return count
+        return count, "..."
     datestring = datetime.now().strftime("%Y-%m-%d %H.%M.%S.%f")
-    image.save(f"./assets/hands/{datestring}.png")
+    image_path = f"./assets/hands/{datestring}.png"
+    image.save(image_path)
     count += 1
-    return count
+    return count, f"Image count: {count}\nLatest image: {image_path}"
 
 def print_images_count(count):
     print(f"Images count: {count}")
     return f"Images count: {count}"
 
 with gr.Blocks() as app:
-    ticker = gr.Timer(value=1, active=True)
-    with gr.Row():
-        with gr.Column():
-            image_input = gr.Image(type="pil", sources="webcam", streaming=True)
-            predict_button = gr.Checkbox(label="Run", every=1)
-        with gr.Column():  
-            images_count = gr.State(0)
-            text_output = gr.Textbox(f"...")
+    ticker = gr.Timer(value=0.1, active=True)
+    with gr.Column():
+        image_input = gr.Image(type="pil", sources="webcam", streaming=True)
+        run_checkbox = gr.Checkbox(label="Run", every=1)
+        images_count = gr.State(0)
+        text_output = gr.Textbox(f"...")
 
-            ticker.tick(fn=save_image, inputs=[predict_button, image_input, images_count], outputs=images_count)
-
-            images_count.change(fn=print_images_count, inputs=images_count, outputs=text_output)
-
+        ticker.tick(fn=save_image, inputs=[run_checkbox, image_input, images_count], outputs=[images_count, text_output])
 
 # app.launch()
 app.launch(server_name="0.0.0.0")
